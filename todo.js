1301657d81816cc7be5b2d55c8dc3c66bcfec8be
waooon <shinobineeena16@gmail.com>
2019-07-30 19:49:53 +0900

    【All】フッター固定 削除ボタンの実装 追加したdivを編集可能にする デザイン微調整

--- todo.js
+++ todo.js
@@ -1,5 +1,27 @@
 function onButtonClick(){
-  var target = document.getElementById("display");
-  target.innerHTML += document.forms.input_todo.inputTextBox.value + "\n";
-  // target.innerText = "bbb";
+
+  var oya = document.createElement("div");
+  oya.classList.add("oya");
+
+  var target = document.createElement("div");
+  target.classList.add("list");
+  target.setAttribute("contenteditable", "true");
+  var text = document.forms.input_todo.inputTextBox.value + "\n"
+
+  var deleteButton = document.createElement("input");
+  deleteButton.setAttribute("type", "button");
+  deleteButton.setAttribute("value", "削除");
+
+  var doneCheckBox = document.createElement("input");
+  doneCheckBox.setAttribute("type", "checkbox");
+
+  var parent_object = document.getElementById("display");
+
+  if(text !== "\n"){
+  target.innerHTML += text.replace(/\r?\n/g, '');
+  parent_object.appendChild(oya);
+  oya.appendChild(target);
+  oya.appendChild(deleteButton);
+  oya.appendChild(doneCheckBox);
+  }
 }
