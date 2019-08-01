863f29ab49a4e5bfc6b3f6b7ee99271100fc2044
waooon <shinobineeena16@gmail.com>
2019-08-01 21:30:59 +0900

    ・テキストボックスEnter押下でリスト追加、テキストボックスクリア、リロードされないように修正 ・チェックボックスにチェックが入っているリストを削除ボタンで削除 ・

--- todo.js
+++ todo.js
@@ -8,20 +8,46 @@ function onButtonClick(){
   target.setAttribute("contenteditable", "true");
   var text = document.forms.input_todo.inputTextBox.value + "\n"
 
-  var deleteButton = document.createElement("input");
-  deleteButton.setAttribute("type", "button");
-  deleteButton.setAttribute("value", "削除");
-
   var doneCheckBox = document.createElement("input");
   doneCheckBox.setAttribute("type", "checkbox");
 
   var parent_object = document.getElementById("display");
 
+  var inputpart = document.getElementsByName("inputTextBox");
+
   if(text !== "\n"){
-  target.innerHTML += text.replace(/\r?\n/g, '');
-  parent_object.appendChild(oya);
-  oya.appendChild(target);
-  oya.appendChild(deleteButton);
-  oya.appendChild(doneCheckBox);
+    target.innerHTML += text.replace(/\r?\n/g, '');
+    parent_object.appendChild(oya);
+    oya.appendChild(target);
+    oya.appendChild(doneCheckBox);
+
+    inputpart[0].value = "";
+  }
+}
+
+function onDeleteButtonClick(){
+
+  var form = document.getElementById("display");
+  var form_child = form.children;
+
+  var object = new Object();
+  for(var i=0; i<form_child.length; i++){
+    // チェックボックス、ラジオボタンはチェックが入ってないものは取得しない
+    if(form_child[i].lastElementChild.type == 'checkbox'){
+      if(!form_child[i].lastElementChild.checked){
+        continue;
+      }
+      else {
+        form.removeChild(form_child[i]);
+        continue;
+      }
+    }
+  }
+}
+
+function enter(){
+  //EnterキーならSubmit
+  if(window.event.keyCode==13){
+    onButtonClick();
   }
 }
